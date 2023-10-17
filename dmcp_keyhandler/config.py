0c26d052b65f826f7b998f18dc2eebc4864eeda6
drz <me@drz.se>
2023-10-17 13:29:37 +0200

    Add files

---
+++ dmcp_keyhandler/config.py
@@ -0,0 +1,14 @@
+class Prod():
+    SECRET_KEY = 'change_me'
+    PASSWORD_HASH = 'change_me'
+    DOVEADM_BIN = '/usr/bin/doveadm'
+
+class Test():
+    SECRET_KEY = 'change_me'
+    PASSWORD_HASH = 'change_me'
+    DOVEADM_BIN = '/usr/bin/doveadm'
+
+class Dev():
+    SECRET_KEY = 'change_me'
+    PASSWORD_HASH = 'change_me'
+    DOVEADM_BIN = '/usr/bin/doveadm'
