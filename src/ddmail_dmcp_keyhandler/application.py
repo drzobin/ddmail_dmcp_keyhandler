8563e610e00362614305413e495e0962c98a8d97
drzobin <me@drz.se>
2025-04-03 22:43:29 +0200

    Refactor to use flask logger

--- src/ddmail_dmcp_keyhandler/application.py
+++ src/ddmail_dmcp_keyhandler/application.py
@@ -21,28 +21,28 @@ def create_key():
 
         # Validate email.
         if is_email_allowed(email) != True:
-            logging.error("create_key() email validation failed")
+            current_app.logger.error("email validation failed")
             return "error: email validation failed"
 
         # Validate _keypassword.
         if is_password_allowed(key_password) != True:
-            logging.error("create_key() key_password validation failed")
+            current_app.logger.error("key_password validation failed")
             return "error: key_password validation failed"
 
         # Validate password.
         if is_password_allowed(password) != True:
-            logging.error("create_key() password validation failed")
+            current_app.logger.error("password validation failed")
             return "error: password validation failed"
 
         # Check if password is correct.
         try:
             if ph.verify(current_app.config["PASSWORD_HASH"], password) != True:
                 time.sleep(1)
-                logging.error("create_key() wrong password")
+                current_app.logger.error("wrong password")
                 return "error: wrong password"
         except VerifyMismatchError:
             time.sleep(1)
-            logging.error("create_key() exceptions VerifyMismatchError, wrong password")
+            current_app.logger.error("exceptions VerifyMismatchError, wrong password")
             return "error: wrong password"
         time.sleep(1)
 
@@ -50,23 +50,23 @@ def create_key():
 
         # Check that doveadm exist.
         if os.path.exists(doveadm) != True:
-            logging.error("create_key() doveadm binary location is wrong")
+            current_app.logger.error("doveadm binary location is wrong")
             return "error: doveadm binary location is wrong"
 
         # Create key with password
         try:
             output = subprocess.run(["/usr/bin/doas",doveadm,"-o","plugin/mail_crypt_private_password="+key_password,"mailbox","cryptokey","generate","-u",email,"-U"], check=True)
             if output.returncode != 0:
-                logging.error("create_key() returncode of cmd doveadm is non zero")
+                current_app.logger.error("returncode of cmd doveadm is non zero")
                 return "error: returncode of cmd doveadm is non zero"
         except subprocess.CalledProcessError as e:
-            logging.error("create_key() returncode of cmd doveadm is non zero")
+            current_app.logger.error("returncode of cmd doveadm is non zero")
             return "error: returncode of cmd doveadm is non zero"
         except:
-            logging.error("create_key() unkown exception running subprocess")
+            current_app.logger.error("unkown exception running subprocess")
             return "error: unkown exception running subprocess"
 
-        logging.debug("create_key() done")
+        current_app.logger.debug("done")
         return "done"
 
 @bp.route("/change_password_on_key", methods=["POST"])
@@ -81,33 +81,33 @@ def change_password_on_key():
 
         # Validate email.
         if is_email_allowed(email) != True:
-            logging.error("change_password_on_key() email validation failed")
+            current_app.logger.error("email validation failed")
             return "error: email validation failed"
 
         # Validate current_key_password.
         if is_password_allowed(current_key_password) != True:
-            logging.error("change_password_on_key() current_key_password validation failed")
+            current_app.logger.error("current_key_password validation failed")
             return "error: current_key_password validation failed"
 
         # Validate new_key_password.
         if is_password_allowed(new_key_password) != True:
-            logging.error("change_password_on_key() new_key_password validation failed")
+            current_app.logger.error("new_key_password validation failed")
             return "error: new_key_password validation failed"
 
         # Validate password.
         if is_password_allowed(password) != True:
-            logging.error("change_password_on_key() password validation failed")
+            current_app.logger.error("password validation failed")
             return "error: password validation failed"
 
         # Check if password is correct.
         try:
             if ph.verify(current_app.config["PASSWORD_HASH"], password) != True:
                 time.sleep(1)
-                logging.error("change_password_on_key() wrong password")
+                current_app.logger.error("wrong password")
                 return "error: wrong password"
         except:
             time.sleep(1)
-            logging.error("change_password_on_key() wrong password")
+            current_app.logger.error("wrong password")
             return "error: wrong password"
         time.sleep(1)
 
@@ -115,21 +115,21 @@ def change_password_on_key():
 
         # Check that doveadm exist.
         if os.path.exists(doveadm) != True:
-            logging.error("change_password_on_key() doveadm binary location is wrong")
+            current_app.logger.error("doveadm binary location is wrong")
             return "error: doveadm binary location is wrong"
 
         # Change password on key.
         try:
             output = subprocess.run(["/usr/bin/doas",doveadm,"mailbox","cryptokey","password","-u",email,"-n",new_key_password,"-o",current_key_password], check=True)
             if output.returncode != 0:
-                logging.error("change_password_on_key() returncode of cmd doveadm is non zero")
+                current_app.logger.error("returncode of cmd doveadm is non zero")
                 return "error: returncode of cmd doveadm is non zero"
         except subprocess.CalledProcessError as e:
-            logging.error("change_password_on_key() returncode of cmd doveadm is non zero")
+            current_app.logger.error("returncode of cmd doveadm is non zero")
             return "error: returncode of cmd doveadm is non zero"
         except:
-            logging.error("change_password_on_key() unkown exception running subprocess")
+            current_app.logger..error("unkown exception running subprocess")
             return "error: unkonwn exception running subprocess"
 
-        logging.debug("change_password_on_key() done")
+        current_app.logger.debug("done")
         return "done"
