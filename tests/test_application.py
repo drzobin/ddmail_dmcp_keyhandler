442978fa6df80fbc2d571863c41354a27a54d845
drzobin <me@drz.se>
2025-04-04 12:42:43 +0200

    Add support for password and config file

--- tests/test_application.py
+++ tests/test_application.py
@@ -2,23 +2,13 @@ from flask import current_app
 import pytest
 import os
 
-def test_hash_data(client):
-    response_hash_data_post = client.post("/hash_data", data={"data":"password"})
-    assert response_hash_data_post.status_code == 200
-    assert b"argon2id" in response_hash_data_post.data
-
-def test_hash_data_illigal_char(client):
-    response_hash_data_post = client.post("/hash_data", data={"data":"-password"})
-    assert response_hash_data_post.status_code == 200
-    assert b"error: validation of data failed" in response_hash_data_post.data
-
 def test_create_key_illigal_char_password(client):
-    response_hash_data_post = client.post("/create_key", data={"password":".password","key_password":"password","email":"test@test.se"})
+    response_hash_data_post = client.post("/create_key", data={"password":".password","key_password":"aDfrdf43DFR432dFtrfde43E","email":"test@test.se"})
     assert response_hash_data_post.status_code == 200
     assert b"error: password validation failed" in response_hash_data_post.data
 
-def test_create_key_illigal_char_key_password(client):
-    response_hash_data_post = client.post("/create_key", data={"password":"password","key_password":"p<assword","email":"test@test.se"})
+def test_create_key_illigal_char_key_password(client,password):
+    response_hash_data_post = client.post("/create_key", data={"password":password,"key_password":"p<assword","email":"test@test.se"})
     assert response_hash_data_post.status_code == 200
     assert b"error: key_password validation failed" in response_hash_data_post.data
 
@@ -28,12 +18,12 @@ def test_create_key_illigal_char_email(client):
     assert b"error: email validation failed" in response_hash_data_post.data
 
 def test_create_key_wrong_password(client):
-    response_hash_data_post = client.post("/create_key", data={"password":"wrongpassword","key_password":"password","email":"test@test.se"})
+    response_hash_data_post = client.post("/change_password_on_key", data={"password":"adFrd34fd34rFDert4edFTRE","current_key_password":"sd34fgFD34fdERF43edSDFTR","new_key_password":"dDFrdswD34fdSed3fdRtfrtf","email":"test@test.se"})
     assert response_hash_data_post.status_code == 200
     assert b"error: wrong password" in response_hash_data_post.data
 
 def test_change_password_on_key_illigal_char_password(client):
-    response_hash_data_post = client.post("/change_password_on_key", data={"password":".password","current_key_password":"password","new_key_password":"password","email":"test@test.se"})
+    response_hash_data_post = client.post("/change_password_on_key", data={"password":"aSdfrGf345fdrtGFrdFR54.2","current_key_password":"sd34fgFD34fdERF43edSDFTR","new_key_password":"dDFrdswD34fdSed3fdRtfrtf","email":"test@test.se"})
     assert response_hash_data_post.status_code == 200
     assert b"error: password validation failed" in response_hash_data_post.data
 
@@ -42,8 +32,8 @@ def test_change_password_on_key_illigal_char_current_key_password(client):
     assert response_hash_data_post.status_code == 200
     assert b"error: current_key_password validation failed" in response_hash_data_post.data
 
-def test_change_password_on_key_illigal_char_new_key_password(client):
-    response_hash_data_post = client.post("/change_password_on_key", data={"password":"password","current_key_password":"password","new_key_password":"pas%sword","email":"test@test.se"})
+def test_change_password_on_key_illigal_char_new_key_password(client, password):
+    response_hash_data_post = client.post("/change_password_on_key", data={"password":password,"current_key_password":"aSdf3fde34fDFR345fdeFDRT","new_key_password":"pas%sword","email":"test@test.se"})
     assert response_hash_data_post.status_code == 200
     assert b"error: new_key_password validation failed" in response_hash_data_post.data
 
@@ -53,6 +43,6 @@ def test_change_password_on_key_illigal_char_email(client):
     assert b"error: email validation failed" in response_hash_data_post.data
 
 def test_change_password_on_key_wrong_password(client):
-    response_hash_data_post = client.post("/change_password_on_key", data={"password":"wrongpassword","current_key_password":"password","new_key_password":"password","email":"test@test.se"})
+    response_hash_data_post = client.post("/change_password_on_key", data={"password":"A3D4fEf3D3F45gFds23F4gfR","current_key_password":"aDfD3fdFd3rFDs345FdsFrdf","new_key_password":"aDfrGf34fdFgt54fdFgfrdfT","email":"test@test.se"})
     assert response_hash_data_post.status_code == 200
     assert b"error: wrong password" in response_hash_data_post.data
